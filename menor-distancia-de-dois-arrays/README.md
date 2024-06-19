4cf1929f9981f57d9b3031543a5846e16584188f
cleverson <cleverson2020333@gmail.com>
2024-06-19 11:19:09 -0300

    finalização do desafio menor distancia de dois arrays

---
+++ menor-distancia-de-dois-arrays/README.md
@@ -0,0 +1,25 @@
+## Desafio menor distância de dois arrays
+João estava participando de uma competição de programação e lhe foi dado um problema em que ele tinha que encontrar a menor distância entre dois números de dois arrays.
+
+Um exemplo seria:
+
+Array 1 -> [-1, 5]
+Array 2 -> [26, 6]
+A menor distância seria a combinação do número 5 do array 1 com o número 6 do array 2, que seria 1 de distância.
+
+Instruções
+Use arrays com tamanho maiores ou iguais a 10 números.
+
+
+## Como executar o código
+
+ ### [👉 Clique aqui para executar o código online 👈](https://replit.com/@cleversonsilvac/Menor-distancia-de-dois-arrays#main.cpp)
+
+ > **obs:** para poder executar o código online, clique no botão **"Fork"**
+   ![](https://imgur.com/GlaR9GL.png)
+
+
+
+
+
+ 
---
+++ menor-distancia-de-dois-arrays/menor_distancia_de_dois_arrays.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+
+
+int partition(int arr[],int low,int high){
+   int pivot=arr[high];
+   int i=(low-1);
+   
+   for(int j=low;j<=high-1;j++){
+      if(arr[j]<pivot){
+         i++;
+         swap(arr[i],arr[j]);
+      }
+   }
+   swap(arr[i+1],arr[high]);
+   return (i+1);
+}
+
+
+void quickSort(int arr[],int low,int high){
+   if(low<high){
+      int pi=partition(arr,low,high);
+      quickSort(arr,low,pi-1);
+      quickSort(arr,pi+1,high);
+   }
+}
+
+
+int absoluteValue(int value){
+   if(value < 0){
+      return value * -1;
+   }
+   return value;
+}
+
+
+int findShortestDistance(int arr1[], int arr2[], int lengthArray1, int lengthArray2){
+   int i = 0;
+   int j = 0;
+
+   int shortest_distance = absoluteValue(arr1[i] - arr2[j]);
+
+   while(i < lengthArray1 && j < lengthArray2){
+      int new_distance = absoluteValue(arr1[i] - arr2[j]);
+
+      if(new_distance < shortest_distance){
+         shortest_distance = new_distance;
+      }
+
+      if(arr1[i] < arr2[j]){ 
+         i++; 
+      }else{ 
+         j++; 
+      }
+   }
+
+   return shortest_distance;
+}
+
+
+
+int main(){
+
+   int array1[] = {1, 2, 3, 5, 7};
+   int array2[] = {10, 20, 30, 40};
+   
+   int lengthArray1 = sizeof(array1)/sizeof(array1[0]);
+   int lengthArray2 = sizeof(array2)/sizeof(array2[0]);
+
+   quickSort(array1,0,lengthArray1-1);
+   quickSort(array2,0,lengthArray2-1);
+
+   int shortest_distance = findShortestDistance(array1,array2, lengthArray1, lengthArray2);
+
+   cout << "A menor distancia entre o 1° array e o 2° array é " << shortest_distance;
+
+   return 0;
+}
